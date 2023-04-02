701705a485ba870549cdd750f9d49b49d7806cfb
doragi <wodls0125@gachon.ac.kr>
2023-04-02 23:48:24 +0900

    23/04/02 23:48 At Army. Coudln't install Unity.

---
+++ Assets/Scripts/Scanner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scanner : MonoBehaviour
+{
+    public float scanRange;
+    public LayerMask targetLayer;
+    public LaycastHit2D[] targets;
+    public Transform nearestTarget;
+
+    void FixedUpdate()
+    {
+        targets = Physics2D.CircleCastAll(transform.position, scanRange, Vector2.zero, 0, targetLayer);
+        nearestTarget = GetNearest();
+    }
+
+    Transform GetNearest()
+    {
+        Transform result = null;
+        float diff = 100;
+
+        foreach (RaycastHit2D target in targets)
+        {
+            Vector3 myPos = transform.position; // 플레이어 위치
+            Vector3 targetPos = targets.transform.position; // 타겟 배열속 각 타겟의 위치
+            float curDiff = Vector3.Distance(myPos, targetPos);
+
+            if (curDiff < diff)
+            {
+                diff = curDiff; // 현재 거리를 업데이트 하고,
+                result = target.transform; // 타겟 지정
+            }
+        }
+
+        return result;
+    }
+}
